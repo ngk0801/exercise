429cea9f8c46e83436f47a3cea865b3c17c90256
ngk0801 <145485753+ngk0801@users.noreply.github.com>
2026-01-13 20:41:37 +0700

    initial commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,22 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Ex10 } from './ex10/ex10';
+
+@NgModule({
+  declarations: [
+    App,
+    Ex10
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
