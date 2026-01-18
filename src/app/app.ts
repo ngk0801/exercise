41fc4c4f0d6e8e23f18c11511f385511bc811e2b
ngk0801 <145485753+ngk0801@users.noreply.github.com>
2026-01-18 19:24:32 +0700

    ex10

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Ex10Component } from './ex10/ex10';
+import { Ex14 } from './ex14/ex14';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Ex10Component],
+  imports: [Ex10Component, Ex14],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
